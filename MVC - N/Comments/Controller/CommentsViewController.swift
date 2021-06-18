1e9de1b40d91597cd362120c01e82e6823a8ab1d
Olesia <odobrogor@gmail.com>
2021-06-18 15:00:43 +0300

    Create Model

---
+++ MVC - N/Comments/Controller/CommentsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MVC - N
+//
+//  Created by Олеся Егорова on 18.06.2021.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+}
+
+extension CommentsViewController: UITableViewDelegate {
+    
+}
+extension CommentsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
---
+++ MVC - N/Comments/Model/Comment.swift
@@ -0,0 +1,16 @@
+//
+//  Comment.swift
+//  MVC - N
+//
+//  Created by Олеся Егорова on 18.06.2021.
+//
+
+import Foundation
+
+struct Comment {
+    var postId: Int
+    var id: Int
+    var name: String
+    var email: String
+    var body: String
+}
